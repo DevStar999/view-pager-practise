253314bbae7ef68adfb446decbc45e0354745dcf
DevStar999 <wolverinerage99@gmail.com>
2023-02-14 16:25:49 +0530

    Implemented viewPager2 with a nice swipe animation

--- app/src/main/java/com/example/viewpagerpractise/MainActivity.java
+++ app/src/main/java/com/example/viewpagerpractise/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.viewpagerpractise;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
 public class MainActivity extends AppCompatActivity {
+    private ViewPager2 viewPager2;
+    private FragmentStateAdapter pagerAdapter;
+
+    private void initialise() {
+        viewPager2 = findViewById(R.id.view_pager);
+        pagerAdapter = new ViewPagerAdapter(this);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initialise();
+
+        viewPager2.setAdapter(pagerAdapter);
+        viewPager2.setPageTransformer(new ZoomOutPageTransformer());
     }
 }
---
+++ app/src/main/java/com/example/viewpagerpractise/ViewPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.example.viewpagerpractise;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0: {
+                return new FirstFragment();
+            }
+            case 1: {
+                return new SecondFragment();
+            }
+            case 2: {
+                return new ThirdFragment();
+            }
+            default: {
+                return null;
+            }
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
